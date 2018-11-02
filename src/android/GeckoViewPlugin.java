ad8a3d74eafc3adf524484b997fcd9ffa18346e0
Laurentiu Ciobanu |vex <laurentiu@thevexis.me>
2018-11-02 11:52:29 +0200

    initial commit

---
+++ src/android/GeckoViewPlugin.java
@@ -0,0 +1,37 @@
+package labs.vex.cordova.geckoview;
+
+import android.widget.Toast;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+public class GeckoViewPlugin extends CordovaPlugin {
+  private static final String DURATION_LONG = "long";
+  @Override
+  public boolean execute(String action, JSONArray args,
+    final CallbackContext callbackContext) {
+      if (!action.equals("show")) {
+        callbackContext.error("\"" + action + "\" is not a recognized action.");
+        return false;
+      }
+      String message;
+      String duration;
+      try {
+        JSONObject options = args.getJSONObject(0);
+        message = options.getString("message");
+        duration = options.getString("duration");
+      } catch (JSONException e) {
+        callbackContext.error("Error encountered: " + e.getMessage());
+        return false;
+      }
+      Toast toast = Toast.makeText(cordova.getActivity(), message,
+        DURATION_LONG.equals(duration) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+      toast.show();
+      PluginResult pluginResult = new PluginResult(PluginResult.Status.OK);
+      callbackContext.sendPluginResult(pluginResult);
+      return true;
+  }
+}
---
+++ www/gecko-view-plugin.js
@@ -0,0 +1,17 @@
+function ToastyPlugin() {}
+
+ToastyPlugin.prototype.show = function(message, duration, successCallback, errorCallback) {
+  var options = {};
+  options.message = message;
+  options.duration = duration;
+  cordova.exec(successCallback, errorCallback, 'ToastyPlugin', 'show', [options]);
+}
+
+ToastyPlugin.install = function() {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+  window.plugins.toastyPlugin = new ToastyPlugin();
+  return window.plugins.toastyPlugin;
+};
+cordova.addConstructor(ToastyPlugin.install);
